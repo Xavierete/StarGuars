ffb5e4305eee048a5d0e4546676aa64b4156b75d
Xavier <152275208+Xavierete@users.noreply.github.com>
2025-03-12 18:43:13 +0100

    Delete StarGuars directory

--- StarGuars/path/to/your/file.js
+++
@@ -1,8 +0,0 @@
-// Modificar el tamaño de afectación del laser
-laserEffect.size = laserEffect.size / 2; // Reducir a la mitad
-
-// Aumentar el tamaño de píxeles de la línea roja
-redLine.pixelSize = redLine.pixelSize * 2; // Aumentar tamaño de píxeles
-
-// Aumentar el tamaño de píxeles de la línea naranja
-orangeLine.pixelSize = orangeLine.pixelSize * 2; // Aumentar tamaño de píxeles 
